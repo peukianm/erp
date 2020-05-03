c25290bfd6d5b15d010959545b36f10d476123d9
user <user@192.168.56.1>
2020-05-03 15:09:01 +0300

    Commit 3/5/2020

--- src/erp/util/PersistenceUtil.java
+++ src/erp/util/PersistenceUtil.java
@@ -7,26 +7,34 @@ import java.math.BigDecimal;
 import java.util.Date;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Stateless
 public class PersistenceUtil {
 
     @EJB
     private PersistenceHelper persistenceHelper;
+    
+    private static final Logger logger = LogManager.getLogger(PersistenceUtil.class);
 
     public void audit(Users user, BigDecimal actionID,  String comments) throws Exception {
         try {
             Action action = (Action) persistenceHelper.find(Action.class, actionID);
             Auditing auditing = new Auditing();
             auditing.setUsers(user);
-
-            auditing.setActiondate(FormatUtils.formatDateToTimestamp(new Date(), FormatUtils.FULLDATEPATTERN));
+            if (logger.isDebugEnabled()) {
+                logger.debug("AUDITING");
+            }
+             
+            
+            auditing.setActiondate(FormatUtils.formatDateToTimestamp(new Date(), FormatUtils.DATEPATTERN));
+            auditing.setActiontime(FormatUtils.formatDateToTimestamp(new Date(), FormatUtils.FULLDATEPATTERN));
             auditing.setAction(action);
             if (user.getCompany()!=null)
                 auditing.setCompany(user.getCompany());
             
-            auditing.setComments(comments);
-           
+            auditing.setComments(comments);                    
             persistenceHelper.create(auditing);
 
         } catch (Exception e) {
